fd6ca38f981d6d97d698ff70b61e8c528dc879e4
Charles de Vandière <charlesdevandiere@users.noreply.github.com>
2023-08-24 10:44:03 +0200

    update sample dependencies (#40)

--- sample/Models/Attack.cs
+++ sample/Models/Attack.cs
@@ -1,20 +1,16 @@
 using System.Text.Json.Serialization;
-using Newtonsoft.Json;
 
 namespace Shared.Models;
 
 public class Attack
 {
     [JsonPropertyName("name")]
-    [JsonProperty("name")]
     public string Name { get; set; }
 
     [JsonPropertyName("type")]
-    [JsonProperty("type")]
     public string Type { get; set; }
 
     [JsonPropertyName("damage")]
-    [JsonProperty("damage")]
     public int Damage { get; set; }
 
     public override string ToString()

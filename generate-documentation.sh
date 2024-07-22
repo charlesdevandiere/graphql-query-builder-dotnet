2c5ba87b15f243f6d76563ffe47be4189d27b4ce
Charles de Vandière <charlesdevandiere@users.noreply.github.com>
2024-07-22 09:24:19 +0200

    shell

--- generate-documentation.sh
+++ generate-documentation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 dotnet tool restore
-dotnet build src/GraphQL.Query.Builder/GraphQL.Query.Builder.csproj -c Release
-dotnet tool run xmldoc2md src/GraphQL.Query.Builder/bin/Release/netstandard2.0/GraphQL.Query.Builder.dll docs/api --github-pages --back-button
+dotnet build src/GraphQL.Query.Builder/GraphQL.Query.Builder.csproj -c Release -o out/
+dotnet tool run xmldoc2md out/GraphQL.Query.Builder.dll docs/api --github-pages --back-button
